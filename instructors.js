85cded7ff919350b938f8af6e3290b4f2b71fcde
Daniel de Paula <daniel.c.paula@hotmail.com>
2020-07-22 23:37:48 -0300

    Função de Data

--- instructors.js
+++ instructors.js
@@ -52,6 +52,21 @@ exports.show = function(req, res) {
   return res.send(foundInstructor)
 }
 
+function age(timestamp) {
+  const today = new Date()
+  const birthDate = new Date(timestamp)
+
+  //2019 - 1984
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const month = today.getMonth() - birthDate.getMonth()
+
+  if(month < 0 || month == 0 && today.getDate() < birthDate.getDate() ) {
+    age = age - 1
+  }
+
+  return age
+}
+
 
 
 

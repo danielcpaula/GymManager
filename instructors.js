13b7c1b932634e5e9c7e251e029647764b70a81b
Daniel de Paula <daniel.c.paula@hotmail.com>
2020-07-26 19:55:53 -0300

    Preenchendo os campos automaticos do instrutor

--- instructors.js
+++ instructors.js
@@ -1,6 +1,42 @@
 const fs = require('fs')
 const data = require("./data.json")
 const { age } = require("./utils")
+
+//show
+exports.show = function(req, res) {
+  //req.query.id
+  //req.body
+  //req.params.id = /:id
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return id == instructor.id
+  })
+
+  if (!foundInstructor) return res.send("Instructor no found")
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    gender: "",
+    services: foundInstructor.services.split(","),
+    created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
+  }
+  //return res.send(foundInstructor)
+  return res.render("instructors/show", { instructor })
+}
+
+//Edit
+exports.edit = function(req, res) {
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return id == instructor.id
+  })
+
+  if (!foundInstructor) return res.send("Instructor no found")
+  return res.render('instructors/edit', {instructor: foundInstructor})
+}
 //Create
 exports.post = function(req, res){
   const keys = Object.keys(req.body)
@@ -37,37 +73,3 @@ exports.post = function(req, res){
   })
   //return res.send(req.body)
 }
-
-exports.show = function(req, res) {
-  //req.query.id
-  //req.body
-  //req.params.id = /:id
-  const { id } = req.params
-
-  const foundInstructor = data.instructors.find(function(instructor){
-    return id == instructor.id
-  })
-
-  if (!foundInstructor) return res.send("Instructor no found")
-
-  const instructor = {
-    ...foundInstructor,
-    age: age(foundInstructor.birth),
-    gender: "",
-    services: foundInstructor.services.split(","),
-    created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
-  }
-  //return res.send(foundInstructor)
-  return res.render("instructors/show", { instructor })
-}
-
-
-
-
-
-
-
-//update
-
-
-//delete

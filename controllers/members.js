88de9763467dc299212baacea40290537adb3b9a
Daniel de Paula <daniel.c.paula@hotmail.com>
2020-07-28 22:42:34 -0300

    Ajustando dados de cadastro de novo membro

--- controllers/members.js
+++ controllers/members.js
@@ -65,22 +65,20 @@ exports.post = function(req, res){
     }
   }
 
-  let {avatar_url, birth, name, services, gender} = req.body
-  
-  birth = Date.parse(birth)
-  const created_at = Date.now()
-  const id = Number(data.members.length + 1)
+  birth = Date.parse(req.body.birth)
+  let id = 1 
+  const lastMember = data.members[data.members.length - 1]
+
+  if(lastMember) {
+    id = lastMember.id + 1
+  }
 
 
   //[]
   data.members.push({
     id,
-    avatar_url,
-    name,
-    birth,
-    gender,
-    services,
-    created_at
+    ...req.body,
+    birth
   })
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
